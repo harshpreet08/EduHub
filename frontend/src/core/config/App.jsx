119a35d6654e421e3f161acb391f6ff6cd11315c
Harshpreet Singh <96812198+harshpreet08@users.noreply.github.com>
2024-02-18 22:33:14 -0400

    adding navbar

--- frontend/src/core/config/App.jsx
+++ frontend/src/core/config/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
+import NavBar from "./Components/NavBar";
+import "./Components/Global.css";
 
 function App() {
-  return (
-    <div>
-      Hi there
-    </div>
-  );
+  return <NavBar />;
 }
 
 export default App;
