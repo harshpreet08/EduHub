c8f8d938998cc0cbe7e38349d3accdb61a29b53f
Harshpreet Singh <hr605358@dal.ca>
2024-02-19 13:48:18 -0400

    adding pricing page

--- frontend/src/core/config/App.jsx
+++ frontend/src/core/config/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import "./Components/Global.css";
 
+import PricingPage from "./Components/PricingPage";
+
 function App() {
-  return <NavBar />;
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<PricingPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
