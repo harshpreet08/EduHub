862b34e55382d24d8cb49f3e87c69ec88e2a0ec5
Harshpreet Singh <hr605358@dal.ca>
2024-02-22 20:40:45 -0400

    adding blog details page

--- frontend/src/core/config/App.jsx
+++ frontend/src/core/config/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "../../Components/NavBar";
 import BlogFormPage from "../../Components/BlogFormPage";
 import BlogListPage from "../../Components/BlogListPage";
+import BlogDetailsPage from "../../Components/BlogDetails";
 import "../../Components/Global.css";
 
 import PricingPage from "../../Components/PricingPage";
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<PricingPage />} />
           <Route path="/blogform" element={<BlogFormPage />} />
           <Route path="/bloglist" element={<BlogListPage />} />
+          <Route path="/blog/:id" element={<BlogDetailsPage />} />
         </Routes>
       </div>
     </Router>
