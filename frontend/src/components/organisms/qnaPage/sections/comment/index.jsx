7a924bb34fcf49b5c2adf779bb5467b45892bd99
DISHA ANAND <ds442718@dal.ca>
2024-03-19 17:43:14 -0300

    feat: addef initial render of nested comments

--- frontend/src/components/organisms/qnaPage/sections/comment/index.jsx
+++ frontend/src/components/organisms/qnaPage/sections/comment/index.jsx
@@ -3,25 +3,21 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { message } from 'antd';
-import moment from 'moment';
-// import tick from '../../../../../public/assets/tick.svg';
-import { /* replyToComment, */ getCommentByQid } from './comment.service';
-import {
-  setAllComments,
-  setCommentText,
-} from './slice/commentsSlice';
+import Comment from './replies';
+import { getCommentByQid } from './comment.service';
+import { setComment, setNewCommentText } from './slice/commentsSlice';
 import styles from './comment.module.scss';
 
-const Comment = () => {
+const CommentContainer = () => {
   const { qId } = useParams();
   const dispatch = useDispatch();
-  const allComments = useSelector(state => state.qnaPageReducer.commentReducer.allComment);
-  const commentText = useSelector(state => state.qnaPageReducer.commentReducer.commentText);
+  const comment = useSelector(state => state.qnaPageReducer.commentReducer.comment);
+  const newCommentText = useSelector(state => state.qnaPageReducer.commentReducer.newCommentText);
 
   useEffect(() => {
     getCommentByQid({ qId })
       .then(({ data: allComments }) => {
-        dispatch(setAllComments(allComments));
+        dispatch(setComment(allComments));
       })
       .catch((err) => {
         message.error(err);
@@ -45,72 +41,33 @@ const Comment = () => {
   };
 
   return (
-    <div className={styles.commentsContainer}>
-      <section className={styles.totalAnswers}>
+    <>
+      {/* Post Parent Level Comment */}
+      <section className={styles.totalAnswersSection}>
         <div className={styles.totalAnswers__count}>
-          {(allComments || []).length} answers
+          {(comment?.replies || []).length} answers
         </div>
         <input
           type="text"
           className={styles.commentText}
           placeholder="Add a comment ..."
-          value={commentText}
-          onChange={e => dispatch(setCommentText(e.target.value))}
+          value={newCommentText}
+          onChange={e => dispatch(setNewCommentText(e.target.value))}
         />
         <button
           type="button"
           className={styles.commentButton}
           onClick={handlePostClick}
-          style={{
-            backgroundColor: '#065fd4',
-            color: '#fff',
-            marginTop: '1rem',
-            padding: '0.3rem',
-            borderRadius: '1rem',
-          }}
         >
           Comment
         </button>
       </section>
-
-      <section className={styles.answerSection}>
-        {(allComments || []).map((comment, index) => {
-          const { userName = '', timeStamp = '' } = comment || {};
-          const answeredDate = moment(timeStamp).format(
-            'MMM DD, YYYY [at] HH:mm',
-          );
-          return (
-            // eslint-disable-next-line react/no-array-index-key
-            <div className={styles.answerSegment} key={index}>
-              {/* <div>
-                {selectedAnswer === index && (
-                  <img
-                    src={tick}
-                    alt="selected answer"
-                    height={25}
-                    width={25}
-                  />
-                )}
-              </div> */}
-              <div className={styles.answerCard}>
-                <p className={styles.answerBody}>{comment.comment}</p>
-                <span>Reply</span>
-                <div className={styles.userDetailSection}>
-                  {answeredDate !== 'Invalid date' && (
-                    <div className={styles.answeredDate}>
-                      <span>answered</span>
-                      <span>{answeredDate}</span>
-                    </div>
-                  )}
-                  <div className={styles.answeredBy}>{userName}</div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+      {/* Comment Section */}
+      <section className={styles.commentsSection}>
+        <Comment comment={comment} />
       </section>
-    </div>
+    </>
   );
 };
 
-export default Comment;
+export default CommentContainer;
