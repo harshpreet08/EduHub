aecb7f3fc2211cea5e08a0bf5e91a7a66019b2af
Harshpreet Singh <hr605358@dal.ca>
2024-03-28 18:32:36 -0300

    added support for conditional rendring in navbar and added website logo

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Navbar from '../Components/NavBar';
-import LandingBody from '../Components/landing-body';
-import About from '../Components/about';
+import React from "react";
+import Navbar from "../Components/NavBar";
+import LandingBody from "../Components/landing-body";
+import About from "../Components/about";
 
 const LandingPage = () => (
   <div className="landing-page">
-    <Navbar />
+    <Navbar pages={["Contact Us", "FAQs", "Register", "Login"]} />
     <LandingBody />
     <About />
     {/* <Contact/> */}
