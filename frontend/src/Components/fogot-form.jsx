807cb30cf357ff54c37b33ab287ff5379f584e71
kunal <kn362288@dal.ca>
2024-04-04 06:24:05 +0530

    Change reset password button

--- frontend/src/Components/fogot-form.jsx
+++ frontend/src/Components/fogot-form.jsx
@@ -103,7 +103,7 @@ export const ForgotForm = () => {
             className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
               px-4 py-3 mt-6"
           >
-            Send OTP
+            Send Reset Link
           </button>
         </form>
         <div className="text-center mt-6">
